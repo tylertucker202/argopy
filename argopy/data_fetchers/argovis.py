98c930f7e922648d99bf1dce4b05cdc55488dc70
tylertucker202 <tylertucker202@gmail.com>
2020-06-22 15:54:00 -1000

    adding addional get_WMO class

--- argopy/data_fetchers/argovis.py
+++ argopy/data_fetchers/argovis.py
@@ -6,23 +6,26 @@
 # This is comprised of functions used to query Argovis api
 # query functions either return dictionary objects or error messages.
 
+access_points = ['wmo', 'profile', 'shape'] # query options available
+exit_points = [ 'json', 'dataframe', 'xarray'] # outputs availably by data fetcher
+dataset_ids = ['phy', 'bgc']
+
 import pandas as pd
 import xarray as xr
 import requests
 import pdb
+import json
+import os, sys
+#sys.path.append(os.path.join(os.pardir, 'stores'))
 from proto import ArgoDataFetcherProto
+# sys.path.append('../..')
+# import argopy
+# from argopy.stores import httpstore
 
 class ArgovisDataFetcher(ArgoDataFetcherProto):
     """
     Manage access to Argo data through Argovis
     """
-
-    def __init__(self):
-        self.access_points = ['profile', 'platform', 'shape'] # query options available
-        self.exit_points = [ 'list of dictionaries', 'dataframe', 'xarray'] # outputs availably by data fetcher
-        self.dataset_ids = ['bgc', 'deep', 'core']
-
-
     @staticmethod
     def urlopenjson(url):
         resp = requests.get(url)
@@ -30,6 +33,11 @@ class ArgovisDataFetcher(ArgoDataFetcherProto):
         if not resp.status_code // 100 == 2:
             return "Error: Unexpected response {}".format(resp)
         return resp.json()
+
+    # @staticmethod
+    # def urlopenjson(url):
+    #     with httpstore(timeout=120).open("https://argovis.colorado.edu/catalog/profiles/5904797_12") as of:
+    #         return json.load(of)
 
     @staticmethod
     def filter_data_mode(profiles, data_mode):
@@ -130,6 +138,29 @@ class ArgovisDataFetcher(ArgoDataFetcherProto):
         profile = self.urlopenjson(url)
         return profile
 
+    def get_profiles(self, profList, presRange=None):
+        '''
+        Parameters
+        ----------
+        profList: list of strings comprising profile numbers (ex 1900722_1)
+        presRange (optional): list of two numbers formatted as [lowerPressure,upperPressure].
+        Example
+        -------
+        from argovis import ArgovisDataFetcher
+        adf = ArgovisDataFetcher()
+        ids=['1900722_1','1900722_2']
+        presRange=[0,20]
+        profiles = adf.get_profiles(ids, presRange)
+
+        '''
+        url = 'https://argovis.colorado.edu/catalog/mprofiles/?'
+        url += str(profList).replace(' ', '')
+        if presRange:
+            pressRangeQuery = '&presRange=' + str(presRange).replace(' ', '')
+            url += pressRangeQuery
+        profiles = self.urlopenjson(url)
+
+
     def get_platform_profiles(self, platform_number):
         '''
         Returns either a list of dictionary objects representing profile data from a platform, or an error string.
@@ -187,4 +218,55 @@ class ArgovisDataFetcher(ArgoDataFetcherProto):
             print(url)
         selectionProfiles = self.urlopenjson(url)
         return selectionProfiles
+
+class Fetch_wmo(ArgovisDataFetcher):
+    def init(self, WMO, CYC=[], presRange=None):
+        self.WMO = WMO
+        self.CYC = CYC
+        self.presRange = presRange
+
+        if isinstance(WMO, int):
+            WMO = [WMO]  # Make sure we deal with a list
+        if isinstance(CYC, int):
+            CYC = np.array((CYC,), dtype='int')  # Make sure we deal with an array of integers
+        if isinstance(CYC, list):
+            CYC = np.array(CYC, dtype='int')  # Make sure we deal with an array of integers
+
+        return self
+
+    
+    @staticmethod
+    def make_profile_id(WMO, CYC):
+        return wmo + '_' + cyc
+
+    def generate_queries(self):
+        '''
+        determine which argovis query should be made: get_profile(), get_platform_profiles(),
+        or get_profiles_by_id()
+        '''
+        if len(self.CYC) == 1: # profile
+            # either get_profile or profiles
+            profiles = self.get_profile(this.make_profile_id(self.WMO[0], self.CYC[0]))
+        elif len(self.CYC) > 1:
+            # get a list of profiles
+            profIds = self.make_list_of_profiles()
+            profiles = self.get_profiles(profIds, self.presRange)
+        else:
+            #get platforms
+            profiles = self.get_platform_profiles(self.WMO)
+        return fun
+    
+    def make_list_of_profiles(self):
+        '''
+        cartesian product of cycle number and wmo number
+        '''
+        profIds = [str(wmo) + '_' + str(cyc) for wmo in self.WMO for cyc for self.CYC]
+        return profIds
+
+
+
+
+class Fetch_box(ArgovisDataFetcher):
+    def __init__(self, BOX)
+        self lon_min
 
